0c30002040ec02288f91954675e077e1dc44719c
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-28 16:02:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
@@ -27,6 +30,49 @@ class App extends StatelessWidget {
   }
 }
 
+@immutable
+class Person {
+  final String uuid;
+  final String name;
+  final int age;
+
+  Person({
+    String? uuid,
+    required this.name,
+    required this.age,
+  }) : uuid = uuid ?? const Uuid().v4();
+
+  Person updated([String? name, int? age]) => Person(
+        uuid: uuid,
+        name: name ?? this.name,
+        age: age ?? this.age,
+      );
+
+  String get displayName => '$name ($age years old)';
+
+  @override
+  bool operator ==(covariant Person other) => uuid == other.uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+
+  @override
+  String toString() => 'Person(uuid: $uuid, name: $name, age: $age)';
+}
+
+class DateModel extends ChangeNotifier {
+  final List<Person> _people = [];
+
+  int get count => _people.length;
+
+  UnmodifiableListView<Person> get people => UnmodifiableListView(_people);
+
+  void addPerson(Person person) {
+    _people.add(person);
+    notifyListeners();
+  }
+}
+
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
@@ -34,7 +80,7 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomePage'),
+        title: const Text('Home Page'),
         centerTitle: true,
       ),
     );
