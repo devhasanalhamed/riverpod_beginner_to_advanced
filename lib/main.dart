ef2a540ce8e90146799134de5b600d9350da187f
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-29 10:43:46 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,157 @@ void main() {
   );
 }
 
+@immutable
+class Film {
+  final String id;
+  final String title;
+  final String description;
+  final bool isFavorite;
+
+  const Film({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.isFavorite,
+  });
+
+  Film copy({required bool isFavorite}) => Film(
+        id: id,
+        title: title,
+        description: description,
+        isFavorite: isFavorite,
+      );
+
+  @override
+  String toString() => 'Film(id: $id, title: $title, '
+      ' description: $description, '
+      'isFavorite: $isFavorite)';
+
+  @override
+  bool operator ==(covariant Film other) =>
+      id == other.id && isFavorite == other.isFavorite;
+
+  @override
+  int get hashCode => Object.hashAll(
+        [
+          id,
+          isFavorite,
+        ],
+      );
+}
+
+const allFilms = [
+  Film(
+    id: '1',
+    title: 'The Shawshank Redemption',
+    description:
+        'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    isFavorite: false,
+  ),
+  Film(
+    id: '2',
+    title: 'The Godfather',
+    description:
+        'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+    isFavorite: false,
+  ),
+  Film(
+    id: '3',
+    title: 'The Dark Knight',
+    description:
+        'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.',
+    isFavorite: false,
+  ),
+  Film(
+    id: '4',
+    title: 'Inception',
+    description:
+        'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea in the mind of a CEO.',
+    isFavorite: false,
+  ),
+  Film(
+    id: '5',
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    description:
+        'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+    isFavorite: false,
+  ),
+  Film(
+    id: '6',
+    title: 'Forrest Gump',
+    description:
+        'The story of a man who witnesses and influences several major historical events in the United States from the late 1950s to the 1990s.',
+    isFavorite: false,
+  ),
+  Film(
+    id: '7',
+    title: 'The Matrix',
+    description:
+        'A computer programmer is awakened from his simulated reality and must confront the truth about his world and himself.',
+    isFavorite: false,
+  ),
+  Film(
+    id: '8',
+    title: 'Pulp Fiction',
+    description:
+        'The lives of two mob hitmen, a boxer, a gangster\'s wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+    isFavorite: false,
+  ),
+  Film(
+    id: '9',
+    title: 'The Silence of the Lambs',
+    description:
+        'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.',
+    isFavorite: false,
+  ),
+  Film(
+    id: '10',
+    title: 'Schindler\'s List',
+    description:
+        'In German-occupied Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazi Germans.',
+    isFavorite: false,
+  ),
+];
+
+class FilmsNotifier extends StateNotifier<List<Film>> {
+  FilmsNotifier() : super(allFilms);
+
+  void update(Film film, bool isFavorite) {
+    // reassign the state will let all the listeners to notify the change direct
+    state = state
+        .map((thisFilm) => thisFilm.id == film.id
+            ? thisFilm.copy(isFavorite: isFavorite)
+            : thisFilm)
+        .toList();
+  }
+}
+
+enum FavoriteStatus {
+  all,
+  favorite,
+  notFavorite,
+}
+
+final favoriteStatusProvider = StateProvider<FavoriteStatus>(
+  (ref) => FavoriteStatus.all,
+);
+
+final allFilmsProvider = StateNotifierProvider<FilmsNotifier, List<Film>>(
+  (ref) => FilmsNotifier(),
+);
+
+final favoriteFilmsProvider = Provider<Iterable<Film>>(
+  (ref) => ref.watch(allFilmsProvider).where(
+        (film) => film.isFavorite,
+      ),
+);
+
+final notFavoriteFilmsProvider = Provider<Iterable<Film>>(
+  (ref) => ref.watch(allFilmsProvider).where(
+        (film) => !film.isFavorite,
+      ),
+);
+
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -37,6 +188,82 @@ class HomePage extends ConsumerWidget {
         title: const Text('HomePage'),
         centerTitle: true,
       ),
+      body: const Column(
+        children: [
+          FilterWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class FilmsWidget extends ConsumerWidget {
+  final AlwaysAliveProviderBase<Iterable<Film>> provider;
+  const FilmsWidget({
+    super.key,
+    required this.provider,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final films = ref.watch(provider);
+
+    return Expanded(
+      child: ListView.builder(
+        itemCount: films.length,
+        itemBuilder: (context, index) {
+          final film = films.elementAt(index);
+          final favoriteIcon = film.isFavorite
+              ? const Icon(Icons.favorite)
+              : const Icon(Icons.favorite_border);
+          return ListTile(
+            title: Text(film.title),
+            subtitle: Text(film.description),
+            trailing: IconButton(
+              onPressed: () {
+                final isFavorite = !film.isFavorite;
+                ref.read(allFilmsProvider.notifier).update(
+                      film,
+                      isFavorite,
+                    );
+              },
+              icon: favoriteIcon,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class FilterWidget extends StatelessWidget {
+  const FilterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        return DropdownButton(
+          value: ref.watch(favoriteStatusProvider),
+          items: FavoriteStatus.values
+              .map(
+                (fs) => DropdownMenuItem(
+                  value: fs,
+                  child: Text(
+                    fs.toString().split('.').last,
+                  ),
+                ),
+              )
+              .toList(),
+          onChanged: (FavoriteStatus? fs) {
+            ref
+                .read(
+                  favoriteStatusProvider.notifier,
+                )
+                .state = fs!;
+          },
+        );
+      },
     );
   }
 }
