6e61eb444add0ec17f1ca845403275a37866aa6d
Hasan Al-Hamed <81860602+devhasanalhamed@users.noreply.github.com>
2024-07-24 15:09:48 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,175 @@
 import 'dart:collection';
+import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:uuid/uuid.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Application',
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.dark,
+      home: const HomePage(),
+    );
+  }
+}
+
+// search for immutable
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String uuid;
+
+  Person({
+    required this.name,
+    required this.age,
+    String? uuid,
+  }) : uuid = uuid ?? const Uuid().v4();
+
+  Person updated([String? name, int? age]) => Person(
+        name: name ?? this.name,
+        age: age ?? this.age,
+        uuid: uuid,
+      );
+
+  String get getName => "$name, ($age years old)";
+
+  @override
+  bool operator ==(covariant Person other) => uuid == other.uuid;
+
+  // usually you hash the values you compare with in the equivalent.
+  @override
+  int get hashCode => uuid.hashCode;
+
+  @override
+  String toString() => "Person(name: $name, age: $age, uuid: $uuid)";
+}
+
+// change notifier is one of the most powerful object to build data model that can have
+// listeners, store complex data, it yield the change but do not specify what changed!
+// no need to specify what data we are storing like generics like state notifier.
+
+class DataModel extends ChangeNotifier {
+  final List<Person> _people = [];
+
+  // to reach the count of people in the data model, fast and efficient.
+  int get count => _people.length;
+
+  // using unmodifiable list view from collections library to reach the people in public
+  UnmodifiableListView<Person> get people => UnmodifiableListView(_people);
+
+  void addPerson(Person person) {
+    _people.add(person);
+    notifyListeners();
+  }
+
+  void removePerson(Person person) {
+    _people.remove(person);
+    notifyListeners();
+  }
+
+  void updatePerson(Person updatedPerson) {
+    final index = _people.indexOf(updatedPerson);
+    final oldPerson = _people[index];
+    if (oldPerson.name != updatedPerson.name ||
+        oldPerson.age != updatedPerson.age) {
+      _people[index] = oldPerson.updated(
+        updatedPerson.name,
+        updatedPerson.age,
+      );
+      notifyListeners();
+    }
+  }
+}
+
+final peopleProvider = ChangeNotifierProvider(
+  (ref) => DataModel(),
+);
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
+
+final nameController = TextEditingController();
+final ageController = TextEditingController();
+
+Future<Person?> createOrUpdatePersonDialog(BuildContext context,
+    [Person? existingPerson]) {
+  String? name = existingPerson?.name;
+  int? age = existingPerson?.age;
+
+  nameController.text = name ?? "";
+  ageController.text = age.toString() ?? "";
+
+  return showDialog<Person?>(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: const Text('Create a person'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: "Enter name here...",
+                  ),
+                  onChanged: (value) => name = value,
+                ),
+                TextField(
+                  controller: ageController,
+                  decoration: InputDecoration(
+                    labelText: "Enter age here...",
+                  ),
+                  onChanged: (value) => age = int.tryParse(value),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("Cancel"),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(
+                  Person(
+                    name: name!,
+                    age: age!,
+                  ),
+                ),
+                child: const Text("Create"),
+              ),
+            ],
+          ));
+}
+
+/*
+import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -225,3 +396,5 @@ Future<Person?> createOrUpdatePersonDialog(
     },
   );
 }
+
+*/
